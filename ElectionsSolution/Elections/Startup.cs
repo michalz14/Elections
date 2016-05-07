69b0840ac8389b13049a4515fe32b3b16e1c4eee
Michał Zieliński <z.zielinskimichal@gmail.com>
2016-05-07 17:13:40 +0200

    Solution

---
+++ ElectionsSolution/Elections/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Elections.Startup))]
+namespace Elections
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
