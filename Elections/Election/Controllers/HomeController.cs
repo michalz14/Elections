cc3f17cadbd9add76666cf1490b976a9f8cd87f2
Michał Zieliński <z.zielinskimichal@gmail.com>
2016-06-04 13:39:56 +0200

    fdsf

---
+++ Elections/Election/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using Election.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Election.Controllers
+{
+    public class HomeController : Controller
+    {
+        private masterEntities db = new masterEntities();
+        public ActionResult Index()
+        {
+            
+            return View();
+        }
+
+        public ActionResult LoginView()
+        {
+            return View();
+        }
+
+        public ActionResult TokenView()
+        {
+            return View();
+        }
+        public ActionResult VoteView()
+        {
+            return View(db.Candidates.ToList());
+        }
+        public ActionResult ThanksPage()
+        {
+            return View();
+        }
+    }
+}
